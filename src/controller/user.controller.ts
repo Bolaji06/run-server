be93d3daed50b56c7d9d63cd14177b772e5d5912
Bolaji06 <joshspencer0603@gmail.com>
2024-09-10 04:29:01 +0100

    create endpoint to get all user and a specific user

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,4 +1,5 @@
 import { Context } from "hono";
+import prisma from "../lib/prisma";
 
 export async function getAllUser(c: Context){
     const id = c.req.param("id");
@@ -11,5 +12,28 @@ export async function getAllUser(c: Context){
     }catch(err){
         console.log(err);
         return c.json({ success: false, message: 'internal server error'}, 500)
+    }
+}
+
+export async function getUser(c: Context){
+    const id = c.req.param("id");
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        })
+        if (!user){
+            return c.json({ success: false, message: 'user not found' }, 404);
+        }
+        return c.json({ success: true, user }, 200)
+        
+    }catch(error){
+        if (error instanceof Error){
+            return c.json({ success: false, Error }, 500);
+        }else {
+            return c.json({ success: false, message: 'internal server error' }, 500);
+        }
     }
 }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
-import { getAllUser } from "../controller/user.controller";
+import { getAllUser, getUser } from "../controller/user.controller";
 
 const userRoutes = new Hono();
 
-userRoutes.get('/user/:id', getAllUser);
+userRoutes.get('/user', getAllUser);
+userRoutes.get('/user/:id', getUser);
 
 export default userRoutes;

dff865d888fda5e272b9279de573885811972be6
Bolaji06 <joshspencer0603@gmail.com>
2024-09-13 06:57:58 +0100

    create a controller for new  worksheet and updated sheet

--- src/controller/workspace.controller.ts
+++ src/controller/workspace.controller.ts
@@ -4,7 +4,7 @@ import prisma from "../lib/prisma";
 export async function createWorkSpace(c: Context) {
   const username = c.req.param("username");
   const tokenId = c.get("jwtPayload")?.id;
-  const body = await c.req.json();
+  const { name } = await c.req.json();
 
   if (!tokenId) {
     return c.json({ success: false, message: "Unauthorized" }, 401);
@@ -23,15 +23,55 @@ export async function createWorkSpace(c: Context) {
       return c.json({ success: false, message: "username mismatch" }, 404);
     }
 
-    // find workspace with the title name Untitled-1
+    let workspaceName = name;
+
+    // if there is no workspace name provided we need to provide
+    // a default name of "Untitled-/d+/$"
+
+    if (!workspaceName) {
+      const workspaces = await prisma.workspace.findMany({
+        where: {
+          name: {
+            startsWith: "Untitled",
+          },
+          user: {
+            id: user.id,
+          },
+        },
+        orderBy: {
+          name: "desc",
+        },
+      });
+
+      if (workspaces.length > 0) {
+        // extracting largest number from "Untitled"
+        const latestWorkspace = workspaces[0];
+        const match = latestWorkspace.name.match(/Untitled-(\d+)$/);
+        const number = match ? parseInt(match[1], 10) : 0;
+        workspaceName`Untitled-${number + 1}`;
+      } else {
+        // no Untitled workspace exits start with "Untitled-1"
+        workspaceName = "Untitled-1";
+      }
+    }
 
     const newWorkSpace = await prisma.workspace.create({
       data: {
-        name: body?.name || "Untitled-1",
+        name: workspaceName,
         user: {
           connect: { id: user.id },
         },
+        sheets: {
+          create: [
+            { contents: "" },
+            { contents: "" },
+            { contents: "" },
+          ]
+        }
       },
+      include: {
+        sheets: true,
+      }
     });
     return c.json({ success: true, newWorkSpace }, 201);
   } catch (error) {
@@ -81,3 +121,90 @@ export async function getAllWorkSpace(c: Context) {
     return c.json({ success: false, message: "internal server error" }, 500);
   }
 }
+
+export async function getWorkSpace(c: Context){
+  const workspaceId = c.req.param("workspaceId");
+
+  if (!workspaceId){
+    return c.json({ success: false, message: 'invalid id'}, 401)
+  }
+
+  try{
+    const workspace = await prisma.workspace.findUnique({
+      where: {
+        id: workspaceId
+      },
+      include: {
+        sheets: true,
+        user: true
+      }
+    });
+
+    if(!workspace){
+      return c.json({ success: false, message: 'workspace not found'}, 404);
+    }
+    return c.json({ success: true, workspace }, 200);
+
+  }catch(error){
+    if (error instanceof Error){
+      console.log(error);
+    }
+    return c.json({ success: false, message: 'internal server error' }, 500);
+  }
+}
+
+export async function updateSheets(c: Context){
+  const workspaceId = c.req.param("workspaceId");
+  const tokenId = c.get("jwtPayload")?.id;
+  const { sheets } = await c.req.json();
+
+  if (!tokenId){
+    return c.json({ success: false, message: 'Unauthorized'}, 401)
+  }
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: tokenId
+      }
+    });
+    if (!user){
+      return c.json({ success: false, message: 'user not found'}, 404)
+    }
+   
+    const workspace = await prisma.workspace.findFirst({
+      where: {
+        id: workspaceId,
+        user: {
+          id: user.id
+        },
+      },
+      include: {
+        sheets: true,
+      }
+    })
+    if (!workspace){
+      return c.json({ success: false, message: 'workspace is Unauthorized or found'}, 404)
+    }
+
+    //sheet.id ??
+    const updatePromise = sheets.map( async (sheet: { id: string, content: string}) =>{
+      return prisma.sheet.update({
+        where: {
+          id: sheet.id
+        },
+        data: {
+          contents: sheet.content
+        }
+      })
+    })
+
+    await Promise.all(updatePromise);
+
+    return c.json({ success: true, message: 'sheets updated successfully' });
+  }catch(error){
+    if (error instanceof Error){
+      console.log(error);
+    }
+    return c.json({ success: false, message: 'internal server error' }, 500)
+  }
+  }
--- src/routes/workspace.ts
+++ src/routes/workspace.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { authMiddleware } from "../middleware/authorization";
-import { createWorkSpace, getAllWorkSpace } from "../controller/workspace.controller";
+import { createWorkSpace, getAllWorkSpace, getWorkSpace } from "../controller/workspace.controller";
 
 
 const workSpaceRoutes = new Hono();
 
 workSpaceRoutes.get('/', authMiddleware, getAllWorkSpace);
+workSpaceRoutes.get('/workspaceId', authMiddleware, getWorkSpace);
 workSpaceRoutes.post('/:username', authMiddleware, createWorkSpace);
+workSpaceRoutes.patch()
 
 export default workSpaceRoutes;
